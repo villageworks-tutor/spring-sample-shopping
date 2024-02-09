09058ab09c43ec0e02ca72122d3e38e9f6607f5b
villageworks <villageworks@gmail.com>
2024-02-09 22:41:09 +0900

    ver.1.03: 入力値チェック

--- src/main/java/com/example/demo/controller/AccountController.java
+++ src/main/java/com/example/demo/controller/AccountController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -34,6 +37,45 @@ public class AccountController {
 			@RequestParam(name = "email", defaultValue = "") String email,
 			@RequestParam(name = "password", defaultValue = "") String password,
 			Model model) {
+		// リクエストパラメータの入力値チェック
+		List<String> errors = new ArrayList<>();
+		if (name.isEmpty()) {
+			// 名前の入力値チェック：必須入力エラー
+			errors.add("名前は必須です。");
+		}
+		if (address.isEmpty()) {
+			// 住所の入力値チェック：必須入力エラー
+			errors.add("住所は必須です。");
+		}
+		if (tel.isEmpty()) {
+			// 電話番号の入力値チェック：必須入力エラー
+			errors.add("電話番号は必須です。");
+		}
+		if (email.isEmpty()) {
+			// メールアドレスの入力値チェック：必須入力エラー
+		} else {
+			if (customerRepository.findByEmail(email) instanceof Customer) {
+				// 重複エラー
+				errors.add("登録済のメールアドレスです。");
+			}
+		}
+		if (password.isEmpty()) {
+			// パスワードの入力値チェック：必須入力エラー
+			errors.add("パスワードは必須です。");
+		}
+		
+		// 入力値チェックのエラーの有無で処理を分岐
+		if (errors.size() > 0) {
+			// 入力値エラーがある場合：エラーリストとリクエストパラメータをスコープに登録
+			model.addAttribute("errorList", errors);
+			model.addAttribute("name", name);
+			model.addAttribute("address", address);
+			model.addAttribute("tel", tel);
+			model.addAttribute("email", email);
+			// 画面遷移
+			return "accountForm";
+		}
+		
 		// リクエストパラメータをもとに登録対象の顧客をインスタンス化
 		Customer customer = new Customer(name, address, tel, email, password);
 		// 登録対象顧客のインスタンスを永続化
