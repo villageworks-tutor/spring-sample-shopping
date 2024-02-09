7426ae940a811f973ea8b0da96d282eececf1f27
villageworks <villageworks@gmail.com>
2024-02-10 00:57:10 +0900

    ver.1.04: 妥当性検証ルーチンを追加

--- src/main/java/com/example/demo/controller/AccountController.java
+++ src/main/java/com/example/demo/controller/AccountController.java
@@ -23,7 +23,9 @@ public class AccountController {
 	
 	// 会員登録画面表示
 	@GetMapping("/account")
-	public String create() {
+	public String create(Model model) {
+		// 会員のインスタンスをスコープに登録
+		model.addAttribute("customer", new Customer());
 		// 画面遷移
 		return "accountForm";
 	}
@@ -37,51 +39,75 @@ public class AccountController {
 			@RequestParam(name = "email", defaultValue = "") String email,
 			@RequestParam(name = "password", defaultValue = "") String password,
 			Model model) {
-		// リクエストパラメータの入力値チェック
-		List<String> errors = new ArrayList<>();
-		if (name.isEmpty()) {
-			// 名前の入力値チェック：必須入力エラー
-			errors.add("名前は必須です。");
-		}
-		if (address.isEmpty()) {
-			// 住所の入力値チェック：必須入力エラー
-			errors.add("住所は必須です。");
-		}
-		if (tel.isEmpty()) {
-			// 電話番号の入力値チェック：必須入力エラー
-			errors.add("電話番号は必須です。");
-		}
-		if (email.isEmpty()) {
-			// メールアドレスの入力値チェック：必須入力エラー
-		} else {
-			if (customerRepository.findByEmail(email) instanceof Customer) {
-				// 重複エラー
-				errors.add("登録済のメールアドレスです。");
-			}
-		}
-		if (password.isEmpty()) {
-			// パスワードの入力値チェック：必須入力エラー
-			errors.add("パスワードは必須です。");
-		}
+		// リクエストパラメータをもとに登録対象の顧客をインスタンス化
+		Customer customer = new Customer(name, address, tel, email, password);
+		// リクエストパラメータの妥当性検査
+		List<String> errors = this.validate(customer);
 		
 		// 入力値チェックのエラーの有無で処理を分岐
+		String nextURL = "redirect:/";
 		if (errors.size() > 0) {
 			// 入力値エラーがある場合：エラーリストとリクエストパラメータをスコープに登録
 			model.addAttribute("errorList", errors);
-			model.addAttribute("name", name);
-			model.addAttribute("address", address);
-			model.addAttribute("tel", tel);
-			model.addAttribute("email", email);
-			// 画面遷移
-			return "accountForm";
+			model.addAttribute("customer", customer);
+			// 遷移先の設定
+			nextURL = "accountForm";
+		} else {
+			// 入力値エラーがない場合：登録対象顧客のインスタンスを永続化
+			customerRepository.save(customer);
 		}
 		
-		// リクエストパラメータをもとに登録対象の顧客をインスタンス化
-		Customer customer = new Customer(name, address, tel, email, password);
-		// 登録対象顧客のインスタンスを永続化
-		customerRepository.save(customer);
 		// 画面遷移
-		return "redirect:/";
+		return nextURL;
+
+	}
+	
+	/**
+	 * 入力値の妥当性を検査する
+	 * @param customer 検査対象の入力値が設定された顧客インスタンス 
+	 * @return エラーリスト
+	 */
+	private List<String> validate(Customer customer) {
+		final String ERR_MESSAGE_REQUIRED = "@fieldは必須です。";
+		final String ERR_MESSAGE_DUPLICATED = "登録済みのメールアドレスです。";
+		
+		List<String> errors = new ArrayList<String>();
+		if (!isRequired(customer.getName())) {
+			errors.add(ERR_MESSAGE_REQUIRED.replace("@field", "名前"));
+		}
+		if (!isRequired(customer.getAddress())) {
+			errors.add(ERR_MESSAGE_REQUIRED.replace("@field", "住所"));
+		}
+		if (!isRequired(customer.getTel())) {
+			errors.add(ERR_MESSAGE_REQUIRED.replace("@field", "電話番号"));
+		}
+		if (!isRequired(customer.getPassword())) {
+			errors.add(ERR_MESSAGE_REQUIRED.replace("@field", "パスワード"));
+		}
+		if (!isRequired(customer.getEmail())) {
+			errors.add(ERR_MESSAGE_REQUIRED.replace("@field", "メールアドレス"));
+		} else if (isDuplicated(customer.getEmail())) {
+			errors.add(ERR_MESSAGE_DUPLICATED);
+		}
+		return errors;
+	}
+	
+	/**
+	 * 必須入力チェック
+	 * @param target 検査対象文字列
+	 * @return 空文字列またはnullの場合はfalse、それ以外はtrue
+	 */
+	private boolean isRequired(String target) {
+		return !(target.isEmpty());
+	}
+	
+	/**
+	 * メールアドレスの重複チェック
+	 * @param target 検査対象メールアドレス
+	 * @return すでに登録されたメールアドレスである場合はtrue、それ以外はfalse
+	 */
+	private boolean isDuplicated(String target) {
+		return (customerRepository.findByEmail(target) instanceof Customer);
 	}
 	
 }
---
+++ src/test/java/com/example/demo/common/ValidatorTest.java
@@ -0,0 +1,14 @@
+package com.example.demo.common;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ValidatorTest {
+
+	@Test
+	void test() {
+		fail("まだ実装されていません");
+	}
+
+}
