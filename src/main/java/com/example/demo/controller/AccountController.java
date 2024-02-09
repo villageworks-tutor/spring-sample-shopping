e7b1d69c2518d719d7401c19e55aeab63b427eb7
villageworks <villageworks@gmail.com>
2024-02-09 22:04:58 +0900

    ver.1.02: アカウント登録

--- src/main/java/com/example/demo/controller/AccountController.java
+++ src/main/java/com/example/demo/controller/AccountController.java
@@ -1,17 +1,45 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.entity.Customer;
+import com.example.demo.repository.CustomerRepository;
+
 
 
 @Controller
 public class AccountController {
+	
+	@Autowired
+	CustomerRepository customerRepository;
 	
 	// 会員登録画面表示
 	@GetMapping("/account")
 	public String create() {
 		// 画面遷移
 		return "accountForm";
+	}
+	
+	// 会員登録処理
+	@PostMapping("/account")
+	public String store(
+			@RequestParam(name = "name", defaultValue = "") String name,
+			@RequestParam(name = "address", defaultValue = "") String address,
+			@RequestParam(name = "tel", defaultValue = "") String tel,
+			@RequestParam(name = "email", defaultValue = "") String email,
+			@RequestParam(name = "password", defaultValue = "") String password,
+			Model model) {
+		// リクエストパラメータをもとに登録対象の顧客をインスタンス化
+		Customer customer = new Customer(name, address, tel, email, password);
+		// 登録対象顧客のインスタンスを永続化
+		customerRepository.save(customer);
+		// 画面遷移
+		return "redirect:/";
 	}
 	
 }
